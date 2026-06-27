1f1c7a226a267fab67333b8397dc14e2e98f89eb
Anwesha Goel <anweshagoel@gmail.com>
2026-06-28 02:21:33 +0530

    Add: k-closest-points-to-origin - submission-3

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        answer = points[:k]
+        maxdist = self.findmax(answer, k)
+        
+        for i in range(k, len(points)):
+            temp = self.euc(points[i])
+            if maxdist[1] > temp:
+                answer[maxdist[0]] = points[i]
+                maxdist = self.findmax(answer, k)
+        
+        return answer
+
+    def findmax(self, arr, k):
+        maxdist = [0, -1]
+        for i in range(k):
+            temp = self.euc(arr[i])
+            if temp > maxdist[1]:
+                maxdist[0] = i
+                maxdist[1] = temp
+        return maxdist
+
+    def euc(self, pair):
+        x, y = pair
+        return x**2 + y**2
